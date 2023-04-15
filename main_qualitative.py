0a9f7b830fc5f39f60a4967cf2fd399081dd9802
Dumbcate <daokynguyen2000@gmail.com>
2023-04-16 02:04:38 +0700

    qualitative 3kphish 9kclean dataset

--- main_qualitative.py
+++ main_qualitative.py
@@ -7,12 +7,15 @@ from tqdm import tqdm
 phishing_list = []
 legit_list = []
 with open('dataset/phishtank.json') as f:
+    i=1
     object_list = json.load(f)
     for _object in object_list:
+        if i==3001: break
         phishing_list.append(_object['url'])
+        i += 1
 
 data = read_csv('dataset/majestic_million.csv')
-legit_list = data['Domain'].tolist()[0:120000]
+legit_list = data['Domain'].tolist()[0:9000]
 tlds = []
 with open('tlds.txt', 'r') as f:
     tlds = f.readlines()
@@ -29,16 +32,16 @@ for legit in tqdm(legit_list, total=len(legit_list)):
     data_to_append.append(0)
     dataset.append(data_to_append)
     
-organizer_feature_train = []
-organizer_dataset_train = read_csv('dataset/labeled_public_test_hackathon_vcs_2023.csv')
-train_urls = organizer_dataset_train['x'].tolist()
-labels = organizer_dataset_train['y'].tolist()
-i=0
-for train_url in train_urls:
-    data_to_append = feature_extraction_more_qualitative.generate_data_set(train_url, tlds, popular_urls)
-    data_to_append.append(labels[i])
-    organizer_feature_train.append(data_to_append)
-    i+=1
+#organizer_feature_train = []
+#organizer_dataset_train = read_csv('dataset/labeled_public_test_hackathon_vcs_2023.csv')
+#train_urls = organizer_dataset_train['x'].tolist()
+#labels = organizer_dataset_train['y'].tolist()
+#i=0
+#for train_url in train_urls:
+#    data_to_append = feature_extraction_more_qualitative.generate_data_set(train_url, tlds, popular_urls)
+#    data_to_append.append(labels[i])
+#    organizer_feature_train.append(data_to_append)
+#    i+=1
 
 fields = ['domain','having_IP_address','domain_length','URL_length','use_shortening_service','having_At_symbol','having_double_slash_redirecting','number_of_dashes','number_of_sub_domains','using_weird_ports','having_https_token_in_URL','randomness_of_URL','having_HTML_tag_in_URL','having_brand_name_in_path','having_brand_name_in_domain','entropy_of_special_characters','having_suspicious_words_in_URL','having_TLD_in_domain_name','having_more_than_1_TLD_in_URL','result']
 #with open('dataset/organizer_feature_train.csv', 'w') as f:
@@ -47,12 +50,12 @@ fields = ['domain','having_IP_address','domain_length','URL_length','use_shorten
 #    for data in tqdm(organizer_feature_train, total=len(organizer_feature_train)):
 #        write.writerow(data)
 #
-with open('dataset/qualitative_feature_labeled_public_test_hackathon_vcs_2023_feature.csv', 'w') as f:
-    write = csv.writer(f)
-    write.writerow(fields)
-    for data in tqdm(organizer_feature_train, total=len(organizer_feature_train)):
-        write.writerow(data)
-with open('dataset/qualitative_balanced_dataset.csv', 'w') as f:
+#with open('dataset/qualitative_feature_labeled_public_test_hackathon_vcs_2023_feature.csv', 'w') as f:
+#    write = csv.writer(f)
+#    write.writerow(fields)
+#    for data in tqdm(organizer_feature_train, total=len(organizer_feature_train)):
+#        write.writerow(data)
+with open('dataset/qualitative_3kphish_9kclean_dataset.csv', 'w') as f:
     write = csv.writer(f)
     write.writerow(fields)
     for data in tqdm(dataset, total=len(dataset)):
